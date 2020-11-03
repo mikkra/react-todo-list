acff464b7cd0476471b1183c239ae6d0d6e2bef9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-03 21:12:42 +0000

    Deploy mikkra/react-todo-list to github.com/mikkra/react-todo-list.git:site-source

---
+++ precache-manifest.a6894a9df66a98a88cab472d51d9cbba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e258094b0031281689022423175def",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ab940e3724492bafa0",
+    "url": "/static/css/main.ae8bd077.chunk.css"
+  },
+  {
+    "revision": "cacdcb420b56f560906f",
+    "url": "/static/js/2.cb0f6c7c.chunk.js"
+  },
+  {
+    "revision": "e1ab940e3724492bafa0",
+    "url": "/static/js/main.d3483190.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
